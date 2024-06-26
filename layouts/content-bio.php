ad6e22d7e2299247081d27a92c32a9568414ebff
xkylz1 <fauzansaputra741@gmail.com>
2024-06-26 23:07:21 +0800

    feat: pagination

--- layouts/content-bio.php
+++ layouts/content-bio.php
@@ -1,15 +1,15 @@
-<!-- content bio -->
 <?php
+
 include("../configs/bio-name.php");
 include("../configs/connection.php");
 error_reporting(0);
 
 $user = $_SESSION['U'];
-$sql = mysqli_query($connect, "select * from user where username = '$user'");
+$sql = mysqli_query($connect, "SELECT * FROM user WHERE username = '$user'");
 $data = mysqli_fetch_array($sql);
 
 $iduser = $data['id_user'];
-$bio = mysqli_query($connect, "SELECT * FROM biography JOIN user on user.id_user = biography.id_user WHERE user.id_user = '$iduser'");
+$bio = mysqli_query($connect, "SELECT * FROM biography JOIN user ON user.id_user = biography.id_user WHERE user.id_user = '$iduser'");
 $biodat = mysqli_fetch_array($bio);
 
 if (!isset($_SESSION['U']) and (!isset($_SESSION['P']))) {
@@ -21,11 +21,28 @@ if (!isset($_SESSION['U']) and (!isset($_SESSION['P']))) {
 } else {
     $btn_status = "";
     $hr = "<hr>";
-    $nama = $data2['name'];
+    $nama = $data['name'];
     $image = '<img src="../uploads/' . $biodat['photo'] . '" width="200px">';
     $table_status = "hidden";
 }
-$sql = mysqli_query($connect, "SELECT name, biography, photo FROM user JOIN biography ON user.id_user = biography.id_user;");
+
+// Pagination settings
+$limit = 3; // Number of entries to show in a page.
+if (isset($_GET["page"])) {
+    $page  = $_GET["page"]; 
+} else {
+    $page = 1; 
+};
+$start_from = ($page-1) * $limit;
+
+// Fetch total records
+$result = mysqli_query($connect, "SELECT COUNT(biography.id_bio) FROM biography JOIN user ON user.id_user = biography.id_user");
+$row = mysqli_fetch_row($result);
+$total_records = $row[0];
+$total_pages = ceil($total_records / $limit);
+
+// Fetch records for current page
+$sql = mysqli_query($connect, "SELECT name, biography, photo FROM user JOIN biography ON user.id_user = biography.id_user LIMIT $start_from, $limit");
 
 ?>
 
@@ -40,7 +57,7 @@ $sql = mysqli_query($connect, "SELECT name, biography, photo FROM user JOIN biog
     </thead>
 
     <?php
-    $nomor = 1;
+    $nomor = $start_from + 1;
     while ($data = mysqli_fetch_array($sql)) {
     ?>
         <tbody>
@@ -51,9 +68,28 @@ $sql = mysqli_query($connect, "SELECT name, biography, photo FROM user JOIN biog
                 <td> <img src="../uploads/<?php echo $data['photo'] ?>" width="100px"></td>
             </tr>
         </tbody>
-    <?php $nomor++;
-    } ?>
+    <?php 
+        $nomor++;
+    } 
+    ?>
 </table>
+
+<!-- Pagination Links -->
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <?php if($page > 1){ ?>
+      <li class="page-item"><a class="page-link" href="?page=<?php echo ($page-1); ?>">Previous</a></li>
+    <?php } ?>
+    
+    <?php for ($i=1; $i<=$total_pages; $i++) { ?>
+      <li class="page-item <?php if($page == $i) echo 'active'; ?>"><a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+    <?php } ?>
+    
+    <?php if($page < $total_pages){ ?>
+      <li class="page-item"><a class="page-link" href="?page=<?php echo ($page+1); ?>">Next</a></li>
+    <?php } ?>
+  </ul>
+</nav>
 
 <button class="btn btn-info" <?php echo $btn_status; ?> onclick="location.href='bio-form.php?id=<?php echo $biodat['id_bio']; ?>'">Edit Data</button>
 
--- layouts/content-user.php
+++ layouts/content-user.php
@@ -1,5 +1,3 @@
-<!-- content portofolio -->
-<!-- Ini Content Portfolio -->
 <?php
 if (!isset($_SESSION['U']) and (!isset($_SESSION['P']))) {
     $hidestatus = "hidden";
@@ -9,7 +7,24 @@ if (!isset($_SESSION['U']) and (!isset($_SESSION['P']))) {
     $hr = "<hr>";
 }
 include("../configs/connection.php");
-$sql = mysqli_query($connect, "select * from user");
+
+// Pagination settings
+$limit = 3; // Number of entries to show in a page.
+if (isset($_GET["page"])) {
+    $page  = $_GET["page"]; 
+} else {
+    $page = 1; 
+};
+$start_from = ($page-1) * $limit;
+
+// Fetch total records
+$result = mysqli_query($connect, "SELECT COUNT(id_user) FROM user");
+$row = mysqli_fetch_row($result);
+$total_records = $row[0];
+$total_pages = ceil($total_records / $limit);
+
+// Fetch records for current page
+$sql = mysqli_query($connect, "SELECT * FROM user LIMIT $start_from, $limit");
 
 if (isset($_GET['pesan'])) {
 ?>
@@ -23,7 +38,6 @@ if (isset($_GET['pesan'])) {
 <button class="btn btn-info" <?php echo $hidestatus; ?> onclick="location.href='user-form.php'">Add Data</button>
 <?php echo $hr; ?>
 
-
 <table class="table table-striped">
     <thead>
         <tr>
@@ -35,7 +49,7 @@ if (isset($_GET['pesan'])) {
     </thead>
 
     <?php
-    $nomor = 1;
+    $nomor = $start_from + 1;
     while ($data = mysqli_fetch_array($sql)) {
     ?>
         <tbody>
@@ -44,14 +58,33 @@ if (isset($_GET['pesan'])) {
                 <td> <?php echo $data['name']; ?> </td>
                 <td> <?php echo $data['username']; ?> </td>
                 <td <?php echo $hidestatus; ?>>
-                    <a href="user-form.php?id=<?php echo $data['id_user']; ?>">Edit</a> |
-                    <a href="../layouts/content-user-delete.php?id=<?php echo $data['id_user']; ?>&sessionId=<?php echo $_SESSION['I']; ?>" onclick="return KonfirmasiHapus()">Delete</a>
+                    <a class="btn btn-warning" href="user-form.php?id=<?php echo $data['id_user']; ?>">Edit</a>
+                    <a class="btn btn-danger" href="../layouts/content-user-delete.php?id=<?php echo $data['id_user']; ?>&sessionId=<?php echo $_SESSION['I']; ?>" onclick="return KonfirmasiHapus()">Delete</a>
                 </td>
             </tr>
         </tbody>
-    <?php $nomor++;
-    } ?>
+    <?php 
+        $nomor++;
+    } 
+    ?>
 </table>
+
+<!-- Pagination Links -->
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <?php if($page > 1){ ?>
+      <li class="page-item"><a class="page-link" href="?page=<?php echo ($page-1); ?>">Previous</a></li>
+    <?php } ?>
+    
+    <?php for ($i=1; $i<=$total_pages; $i++) { ?>
+      <li class="page-item <?php if($page == $i) echo 'active'; ?>"><a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+    <?php } ?>
+    
+    <?php if($page < $total_pages){ ?>
+      <li class="page-item"><a class="page-link" href="?page=<?php echo ($page+1); ?>">Next</a></li>
+    <?php } ?>
+  </ul>
+</nav>
 
 <script>
     function alert() {
@@ -65,4 +98,3 @@ if (isset($_GET['pesan'])) {
             return false;
     }
 </script>
-<!-- End of Content Portfolio -->
