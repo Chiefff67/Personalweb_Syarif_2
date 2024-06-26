17c4cfb6f9f8edffd9d481912db21b03c5b6b3e7
xkylz1 <fauzansaputra741@gmail.com>
2024-06-26 21:18:20 +0800

    change: header home

--- layouts/header-home.php
+++ layouts/header-home.php
@@ -1,8 +1,9 @@
 <!-- Header -->
 <div class="jumbotron jumbotron-fluid jumbotron-home">
     <div class="container">
-        <h1 class="display-4">Personal Website Syarif Maulana</h1>
-        <p class="lead">Selamat datang di website pribadi Syarif, anda bisa melihat Biografi, Portfolio, dan Kontak Pribadi.</p>
+        <h1 class="display-4">Fauzan Saputra</h1>
+        <h2>NIM.2211016310003</h2>
+        <p class="lead">I'm excited to share that I'm learning web development with PHP! In my current project, I'm building a CRUD system (Create, Read, Update, Delete) to manage data efficiently. It's a great hands-on experience, and I'm gaining valuable skills along the way!</p>
     </div>
 </div>
 
